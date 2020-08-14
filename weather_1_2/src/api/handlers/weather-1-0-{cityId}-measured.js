b14e84e66500360f9783332c5b0746fa66f7fa85
hikaru zhen <69030114+hikarizhen@users.noreply.github.com>
2020-08-14 11:38:53 +0900

    logic sample

--- weather_1_2/src/api/handlers/weather-1-0-{cityId}-measured.js
+++ weather_1_2/src/api/handlers/weather-1-0-{cityId}-measured.js
@@ -1,5 +1,6 @@
 
 const handler = module.exports = {};
+const { cyan, gray } = require('colors/safe');
 
 /**
  * send measured temperature info to the broker.
@@ -10,6 +11,9 @@ const handler = module.exports = {};
  */
 handler.receiveCityMeasurement = async ({message}) => {
   // Implement your business logic here...
+  console.log(cyan.underline(`receiveCityMeasurement `), gray(' begin'), '\n');
+  console.log(message.payload.temperature + " is collected and will be publish to the broker.");
+
 };
 /**
  * user subscribe the channel
@@ -20,4 +24,6 @@ handler.receiveCityMeasurement = async ({message}) => {
  */
 handler.subscribeCityMeasurement = async ({message}) => {
   // Implement your business logic here...
+  console.log(cyan.underline(`subscribeCityMeasurement `), gray(' begin'), '\n');
+
 };
